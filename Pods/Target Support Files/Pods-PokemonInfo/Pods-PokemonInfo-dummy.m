d2196cb638bc27b6ef7d33c4dbe3e2f504023539
Ali <s.ali@labmc.it>
2020-09-05 17:23:07 +0200

    pods files added

---
+++ Pods/Target Support Files/Pods-PokemonInfo/Pods-PokemonInfo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokemonInfo : NSObject
+@end
+@implementation PodsDummy_Pods_PokemonInfo
+@end
---
+++ Pods/Target Support Files/Pods-PokemonInfoTests/Pods-PokemonInfoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokemonInfoTests : NSObject
+@end
+@implementation PodsDummy_Pods_PokemonInfoTests
+@end
